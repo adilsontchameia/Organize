91e103d8ec5c5bbefd8ae4eea604eea8d1687867
adilsonchameia <adilsonkchameia@hotmail.com>
2021-03-05 19:29:23 +0100

    Recuperando despesas!

--- app/src/main/java/com/adilsonchameia/organize/activity/Despesa.java
+++ app/src/main/java/com/adilsonchameia/organize/activity/Despesa.java
@@ -1,5 +1,6 @@
 package com.adilsonchameia.organize.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,15 +9,25 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.adilsonchameia.organize.R;
+import com.adilsonchameia.organize.config.ConfiguracaoFirebase;
+import com.adilsonchameia.organize.helper.Base64Custom;
 import com.adilsonchameia.organize.helper.DateCustom;
 import com.adilsonchameia.organize.model.Movimentacao;
+import com.adilsonchameia.organize.model.Usuario;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class Despesa extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
     private Movimentacao movimentacao;
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase;
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,5 +88,23 @@ public class Despesa extends AppCompatActivity {
             Toast.makeText(this, "Valor nao preenchido", Toast.LENGTH_SHORT).show();
             return false;
         }
+    }
+
+    public void recuperarDespesaTotal() {
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Usuario usuario = snapshot.getValue(Usuario.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        })
     }
 }
